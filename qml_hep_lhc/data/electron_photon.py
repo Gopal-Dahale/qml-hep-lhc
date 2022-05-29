19c9e9a0ad4d115b72abe0b4ec2f3439d4df9266
Gopal Ramesh Dahale <dahalegopal27@gmail.com>
2022-05-29 10:47:41 +0530

    Removed old code

--- qml_hep_lhc/data/electron_photon.py
+++ qml_hep_lhc/data/electron_photon.py
@@ -11,14 +11,11 @@ class ElectronPhoton(BaseDataModule):
         self.dims = (32,32,1)
         self.output_dims = (1,)
         self.mapping = range(2)
+        self.args['is_binary_data'] = True
+        self.percent_samples = self.args.get("percent_samples", 1.0)
 
         self.filename = self.data_dir / 'electron_photon.npz'
-        self._binary_data = self.args.get("binary_data", None)
-
-        self.percent_samples = self.args.get("percent_samples", 1.0)
         
-        if self._binary_data is not None:
-            raise ValueError("Binary data argument is not supported for electron_photon dataset as it is already binary")
 
     def prepare_data(self):
         if not self.filename.exists():
@@ -27,8 +24,11 @@ class ElectronPhoton(BaseDataModule):
         data = np.load(self.filename, allow_pickle=True)
         self.x_train, self.y_train = data['x_train'], data['y_train']
         self.x_test, self.y_test = data['x_test'], data['y_test']
+        
+        
 
         self.x_train, self.y_train = shuffle(self.x_train, self.y_train)
+        
         
         # extract percent_samples of data from x_train and x_test
         self.x_train = self.x_train[:int(self.percent_samples * len(self.x_train))]
@@ -36,6 +36,7 @@ class ElectronPhoton(BaseDataModule):
         self.x_test = self.x_test[:int(self.percent_samples * len(self.x_test))]
         self.y_test = self.y_test[:int(self.percent_samples * len(self.y_test))]
 
+        
     def setup(self):
 
         preprocessor = DataPreprocessor(data={
@@ -51,11 +52,11 @@ class ElectronPhoton(BaseDataModule):
         self.y_train = preprocessor.y_train
         self.x_test = preprocessor.x_test
         self.y_test = preprocessor.y_test
-
+        
         self.dims = preprocessor.dims
         self.output_dims = preprocessor.output_dims
         self.mapping = preprocessor.mapping
-
+  
         self.encoding_data_to_quantum_circuit()
                     
 
