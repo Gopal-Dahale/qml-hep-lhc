bb3826ccfb6f5e994add5e385a805b9362edc5ee
Gopal Ramesh Dahale <dahalegopal27@gmail.com>
2022-07-03 12:34:30 +0530

    Updated qcnn cong with amplitude encoding

--- qml_hep_lhc/models/quantum/qcnn_cong.py
+++ qml_hep_lhc/models/quantum/qcnn_cong.py
@@ -7,56 +7,75 @@ from qml_hep_lhc.models.base_model import BaseModel
 import tensorflow_quantum as tfq
 import numpy as np
 from qml_hep_lhc.encodings import AngleMap
-from qml_hep_lhc.models.quantum.utils import one_qubit_unitary
+from qml_hep_lhc.utils import _import_class
 from tensorflow import Variable, random_uniform_initializer, constant, shape, repeat, tile, concat, gather
+from qml_hep_lhc.models.quantum.utils import symbols_in_expr_map, resolve_formulas, get_count_of_qubits, get_num_in_symbols
+from tensorflow import pad, constant
+
+CONV_SYMBOLS = 15
+POOL_SYMBOLS = 6
 
 
 class QCNN(Layer):
 
-    def __init__(self, input_dim):
+    def __init__(self, input_dim, fm_class):
         super(QCNN, self).__init__()
 
+        self.fm_class = fm_class
+
         # Prepare qubits
-        self.n_qubits = np.prod(input_dim)
+        self.n_qubits = get_count_of_qubits(self.fm_class, input_dim)
         self.qubits = cirq.GridQubit.rect(1, self.n_qubits)
-        self.observables = [cirq.Z(self.qubits[-1])]
 
-        var_symbols = sympy.symbols(f'θ0:{63}')
-        self.var_symbols = np.asarray(var_symbols).reshape((63))
+        # Observables
+        Z = cirq.PauliString(cirq.Z(self.qubits[-1]))
+        I = cirq.PauliString(cirq.I(self.qubits[-1]))
+        self.observables = [-0.5 * Z + 0.5 * I]
 
-        in_symbols = sympy.symbols(f'x0:{self.n_qubits}')
-        self.in_symbols = np.asarray(in_symbols).reshape((self.n_qubits))
+        # Sympy symbols for variational angles
+        self.num_layers = int(np.log2(self.n_qubits))
+
+        var_symbols = sympy.symbols(
+            f'θ0:{self.num_layers*(CONV_SYMBOLS + POOL_SYMBOLS)}')
+        self.var_symbols = np.asarray(var_symbols).reshape(self.num_layers, -1)
+
+        # Sympy symbols for encoding angles
+        self.num_in_symbols = get_num_in_symbols(self.fm_class, input_dim)
+        in_symbols = sympy.symbols(f'x0:{self.num_in_symbols}')
+        self.in_symbols = np.asarray(in_symbols).reshape((self.num_in_symbols))
 
     def build(self, input_shape):
 
-        circuit = cirq.Circuit()
-        circuit += cluster_state_circuit(self.qubits)
-        fm = AngleMap()
-        circuit += fm.build(self.qubits, self.in_symbols)
+        # Define data and model circuits
+        data_circuit = cirq.Circuit()
+        model_circuit = cirq.Circuit()
 
-        # First convolution layer with pooling layer
-        # Reduces 16 qubits to 8 qubits
-        circuit += quantum_conv_circuit(self.qubits, self.var_symbols[0:15])
-        circuit += quantum_pool_circuit(self.qubits[:8], self.qubits[8:],
-                                        self.var_symbols[15:21])
+        # Prepare data circuit
+        data_circuit += cluster_state_circuit(self.qubits)
+        self.fm = _import_class(f"qml_hep_lhc.encodings.{self.fm_class}")()
+        data_circuit += self.fm.build(self.qubits, self.in_symbols)
 
-        # Second convolution layer with pooling layer
-        # Reduces 8 qubits to 4 qubits
-        circuit += quantum_conv_circuit(self.qubits[8:],
-                                        self.var_symbols[21:36])
-        circuit += quantum_pool_circuit(self.qubits[8:12], self.qubits[12:],
-                                        self.var_symbols[36:42])
+        # Prepare model circuit
+        # Alternating convolutional and pooling layer
+        n = self.n_qubits
+        for i in range(self.num_layers):
+            conv_start = n - (n // 2**i)
+            pool_source_start = conv_start
+            pool_source_end = n - (n // 2**(i + 1))
+            pool_sink_start = pool_source_end
 
-        # Final convoluation layer with pooling layer
-        # Reduces 4 qubits to 1
-        circuit += quantum_conv_circuit(self.qubits[12:],
-                                        self.var_symbols[42:57])
-        circuit += quantum_pool_circuit(self.qubits[12:15], [self.qubits[15]],
-                                        self.var_symbols[57:63])
+            model_circuit += quantum_conv_circuit(
+                self.qubits[conv_start:], self.var_symbols[i, :CONV_SYMBOLS])
+            model_circuit += quantum_pool_circuit(
+                self.qubits[pool_source_start:pool_source_end],
+                self.qubits[pool_sink_start:], self.var_symbols[i,
+                                                                CONV_SYMBOLS:])
 
+        # Convert symbols to list
         self.var_symbols = list(self.var_symbols.flat)
         self.in_symbols = list(self.in_symbols.flat)
 
+        # Initalize variational angles
         var_init = random_uniform_initializer(minval=-np.pi / 2,
                                               maxval=np.pi / 2)
         self.theta = Variable(initial_value=var_init(
@@ -64,26 +83,45 @@ class QCNN(Layer):
                               trainable=True,
                               name="thetas")
 
-        # Define explicit symbol order
-        symbols = [str(symb) for symb in self.var_symbols + self.in_symbols]
-        self.indices = constant([symbols.index(a) for a in sorted(symbols)])
+        # Flatten circuit
+        data_circuit, expr_map = cirq.flatten(data_circuit)
+        self.raw_symbols = symbols_in_expr_map(expr_map)
+        self.expr = list(expr_map)
 
+        # Define explicit symbol order and expression resolver
+        symbols = [str(symb) for symb in self.var_symbols + self.expr]
+        self.indices = constant([symbols.index(a) for a in sorted(symbols)])
+        self.input_resolver = resolve_formulas(self.expr, self.raw_symbols)
+
+        # Define computation layer
         self.empty_circuit = tfq.convert_to_tensor([cirq.Circuit()])
+        self.circuit = data_circuit + model_circuit
         self.computation_layer = tfq.layers.ControlledPQC(
-            circuit, self.observables)
+            self.circuit, self.observables)
 
     def call(self, input_tensor):
+
+        # Pad input tensor to nearest power of 2 in case of amplitude encoding
+        # Padded with one to avoid division by zero
+        padding = self.num_in_symbols - input_tensor.shape[1]
+        if padding:
+            input_tensor = pad(input_tensor,
+                               constant([[0, 0], [0, padding]]),
+                               constant_values=1.0)
+
+        resolved_inputs = self.input_resolver(input_tensor)
         batch_dim = shape(input_tensor)[0]
+
         tiled_up_circuits = repeat(self.empty_circuit,
                                    repeats=batch_dim,
                                    name="tiled_up_circuits")
         tiled_up_thetas = tile(self.theta,
                                multiples=[batch_dim, 1],
                                name="tiled_up_thetas")
-        joined_vars = concat([tiled_up_thetas, input_tensor], axis=-1)
+        joined_vars = concat([tiled_up_thetas, resolved_inputs], axis=1)
         joined_vars = gather(joined_vars,
                              self.indices,
-                             axis=-1,
+                             axis=1,
                              name='joined_vars')
         out = self.computation_layer([tiled_up_circuits, joined_vars])
         return out
@@ -101,19 +139,24 @@ class QCNNCong(BaseModel):
 
         # Data config
         self.input_dim = data_config["input_dims"]
-        self.qcnn = QCNN(self.input_dim)
+
+        self.fm_class = self.args.get("feature_map")
+        if self.fm_class is None:
+            self.fm_class = "AngleMap"
+
+        self.qcnn = QCNN(self.input_dim, self.fm_class)
 
     def call(self, input_tensor):
         """
-				`call` takes in an input tensor, adds the cluster circuit to it, and then passes the result to
-				the expectation layer
-				
-				Args:
-					input_tensor: The input tensor to the layer.
-				
-				Returns:
-					The expectation value of the cluster state.
-				"""
+        `call` takes in an input tensor, adds the cluster circuit to it, and then passes the result to
+        the expectation layer
+        
+        Args:
+            input_tensor: The input tensor to the layer.
+        
+        Returns:
+            The expectation value of the cluster state.
+        """
         x = Flatten()(input_tensor)
         out = self.qcnn(x)
         return out
